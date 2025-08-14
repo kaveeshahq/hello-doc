fb238457b4d8c100ca719381465b9b81ca8b9394
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-14 10:55:36 +0530

    Contineu creating admin dashboard

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { assets } from '../assets/assets';
+import { AdminContext } from '../context/AdminContext';
+import {useNavigate} from 'react-router-dom'
+
+const Navbar = () => {
+  const { aToken ,setAToken } = useContext(AdminContext);
+  const navigate = useNavigate()
+  const logout = () => {
+    navigate('/')
+    aToken && setAToken('')
+    aToken && localStorage.removeItem('aToken')
+  }
+
+  return (
+    <div className="flex justify-between items-center px-6 py-3 bg-gradient-to-r from-primary to-third text-white shadow-md">
+      <div className="flex items-center gap-3">
+        <img src={assets.logo} alt="Logo" className="h-10 rounded-full w-auto" />
+        <p className="font-semibold border px-2.5 py-0.5 rounded-full text-lg">{aToken ? 'Admin' : 'Doctor'}</p>
+      </div>
+      <button onClick={logout} className="bg-secondary text-black font-medium px-4 py-2 rounded-lg hover:bg-primary hover:text-white transition-colors">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,18 +1,102 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { useState } from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { useContext } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import {toast} from 'react-toastify'
 
 const Login = () => {
+  const [state, setState] = useState("Admin");
 
-  const [state,setState] = useState('Admin')  
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (state === "Admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        }else{
+            toast.error(data.message)
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
 
   return (
-    <form>
-      <div>
-        <p><span>{state}</span> Login</p>
+    <form
+      onSubmit={onSubmitHandler}
+      className="min-h-[80vh] flex items-center bg-gray-50"
+    >
+      <div className="flex flex-col gap-4 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-2xl shadow-lg text-sm text-gray-600 bg-white">
+        <p className="text-2xl font-semibold m-auto text-primary">
+          <span>{state}</span> Login
+        </p>
+
+        <div className="w-full">
+          <p className="mb-1 text-primary-dull">Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            required
+            className="w-full p-2 border border-primary-dull rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+          />
+        </div>
+
+        <div className="w-full">
+          <p className="mb-1 text-primary-dull">Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            required
+            className="w-full p-2 border border-primary-dull rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 mt-2 rounded-lg bg-primary text-white font-medium hover:bg-secondary hover:text-black transition-colors"
+        >
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login ?
+            <span
+              className="text-primary cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              {" "}
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login ?{" "}
+            <span
+              className="text-primary cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              {" "}
+              Click here
+            </span>
+          </p>
+        )}
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
