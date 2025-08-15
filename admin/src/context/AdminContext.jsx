b5d7716ce71f6c7ea8da3868512c2ae7afdf9e5f
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-15 15:14:06 +0530

    Complete the Admin Dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider  = (props) => {
 
     const [aToken , setAToken] = useState(localStorage.getItem('aToken') ?localStorage.getItem('aToken'): '' )
     const [doctors,setDoctors] = useState([])
+    const [dashData, setDashData] = useState(false)
     const [appointments, setAppointments] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     
@@ -71,6 +72,21 @@ const AdminContextProvider  = (props) => {
            toast.error(error.message) 
     }
    }
+
+   const getDashData = async () => {
+    try {
+        const {data} = await axios.get(backendUrl + "/api/admin/dashboard" , {headers: {aToken}})
+        if (data.success) {
+            setDashData(data.dashData)
+           console.log(data.dashData);
+           
+        }else{
+            toast.error(data.message)
+        }
+    } catch (error) {
+           toast.error(error.message) 
+    }
+   }
     
     const value = {
         aToken,
@@ -83,6 +99,8 @@ const AdminContextProvider  = (props) => {
         appointments,
         setAppointments,
         cancelAppoinment,
+        getDashData,
+        dashData,
     }
     return(
         <AdminContext.Provider value={value}>
